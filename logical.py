41534eed7833cfb5bad91d9e1b944c2df9945da3
Tenzin Wangchuk <02230087.cst@rub.edu.bt>
2024-03-16 16:00:37 +0530

    i love coding

---
+++ logical.py
@@ -0,0 +1,23 @@
+x = 5
+print(x > 3 and x < 10)
+
+y = 12 
+print(y > 10 and y % 5 == 0)
+
+x = 7
+print(x < 4 or x == 7)
+
+y = 5
+print(y > 10 or y % 2 == 0)
+
+x = 6 
+print(not(x > 7 and x % 4 == 0))
+
+
+print = input("Enter your age:")
+print = input("Are you a student?(yes/no):")
+
+
+
+
+
